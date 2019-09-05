6fa42d7b414bbca4b091741108e95e2c96a7bf2c
divyananjilath <36256270+divyananjilath@users.noreply.github.com>
2019-09-05 22:07:49 +0530

    Create Birthday-Cake-Candles.c

---
+++ Birthday-Cake-Candles.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+int birthdayCakeCandles(int a[],int n)
+{
+    int i,max,count=0;
+    max=a[0];
+    for(i=1;i<n;i++)
+    {
+        if(max<a[i])
+        {
+            max=a[i];
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]==max)
+        {
+           count++;
+        }
+    }
+    return count;
+}
+int main()
+{
+    int n,i,a[1000000];
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+         scanf("%d",&a[i]);
+    }
+    printf("%d",birthdayCakeCandles(a,n));
+    return 0;
+}
